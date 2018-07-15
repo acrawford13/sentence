d70dfdd24426cf8776e334747a4fedd068e5592a
Andrea Crawford <andrea.crawford13@gmail.com>
2018-07-15 14:11:23 +0200

    Add header component

---
+++ src/components/organisms/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #fafafa;
+  padding: 1rem;
+`;
+
+const Heading = styled.h1`
+  color: purple;
+`;
+
+const Header = () => <Wrapper><Heading>Andrea</Heading></Wrapper>;
+
+export default Header;
